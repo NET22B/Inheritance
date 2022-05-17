be1b7f912e66c835ab8e931f0d8d0b0e2a2e9aa1
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-17 14:12:12 +0200

    Person

---
+++ Inheritance/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Person
+    {
+        public string Name { get; set; } 
+    } 
+    
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }  
+    
+    internal class Admin : Employee
+    {
+        public string Department { get; set; }
+    }
+
+
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,7 +6,17 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee employee = new Employee();
+            employee.Salary = 50000;
+            Person person = new Person();
+            person = employee;
+            
+
+            Employee employee2 = (Employee)person;
+            employee2.Salary = 10000;
+            var salary = employee2.Salary;
+
+            Console.WriteLine(employee.Salary);
         }
     }
 }
